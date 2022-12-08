8b3016a1c0d6af09a01d9e7cfcd62f2c6122b7fe
Henry Skorpe Sjøen <henry@kodekameraten.no>
2022-12-08 13:02:27 +0100

    Update file .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,10 @@ module.exports = function(eleventyConfig) {
             ulClass: 'task-list',
             liClass: 'task-list-item'
         })
+        .use(require('markdown-it-plantuml'), {
+            openMarker: '```plantuml',
+            closeMarker: '```'
+        })
         .use(namedHeadingsFilter)
         .use(function(md) {
             //https://github.com/DCsunset/markdown-it-mermaid-plugin
@@ -129,9 +133,12 @@ module.exports = function(eleventyConfig) {
             const title = linkTitle ? linkTitle : fileName;
             let deadLink = false;
 
-
             try {
-                const file = fs.readFileSync(`./src/site/notes/${fileName}.md`, 'utf8');
+                const startPath = './src/site/notes/';
+                const fullPath = fileName.endsWith('.md') ? 
+                    `${startPath}${fileName}`
+                    :`${startPath}${fileName}.md`;
+                const file = fs.readFileSync(fullPath, 'utf8');
                 const frontMatter = matter(file);
                 if (frontMatter.data.permalink) {
                     permalink = frontMatter.data.permalink;
@@ -177,6 +184,9 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/site/img");
     eleventyConfig.addPlugin(faviconPlugin, { destination: 'dist' });
+    eleventyConfig.addFilter('jsonify', function (variable) {
+      return JSON.stringify(variable);
+    });
 
     return {
         dir: {
